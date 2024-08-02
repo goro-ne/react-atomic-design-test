73782a190843e6f370afc1bf1bc5295ec687f34d
goro-ne <hayao56@gmail.com>
2024-08-02 12:55:18 +0900

    UserIconWithNameを追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,19 @@ import './App.css';
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
+import { UserCard } from './components/organisms/user/UserCard';
+
+const user = {
+  name: "山田裕一",
+  image: "https://media.istockphoto.com/id/2153160662/ja/%E3%82%B9%E3%83%88%E3%83%83%E3%82%AF%E3%83%95%E3%82%A9%E3%83%88/young-man-portrait.jpg?s=1024x1024&w=is&k=20&c=Ncb2GPMMauHpHMXNTpqpiDPJJ-_gN8RarJrpTrbbhmc=",
+  email: "yuichi-yamada@test.com",
+  phone: "090-1111-1111",
+  company: {
+    name: "テスト株式会社",
+    website: "https://google.com"
+
+  }
+};
 
 function App() {
   return (
@@ -11,6 +24,7 @@ function App() {
         <SecondaryButton>検索</SecondaryButton>
         <br />
         <SearchInput />
+        <UserCard user={user} />
       </div>
     </>
   );
---
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const UserIconWithName = (props) => {
+    const { image, name } = props;
+    return (
+        <div>
+            <SImgWrapper>
+                <SImg src={image} alt={name} />
+            </SImgWrapper>
+            <SName>{name}</SName>
+        </div>
+    );
+};
+
+const SContainer = styled.div`
+    text-align: center;
+`;
+
+const SImgWrapper = styled.div`
+    width: 160px;
+    height: 160px;
+    overflow: hidden;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;  // 親コンテナの中央に配置
+`;
+
+const SImg = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover; // 画像のアスペクト比を維持しつつ、円形の表示領域全体をカバー
+    border-radius: 50%;
+`;
+const SName = styled.p`
+    font-size: 18px;
+    font-weight: bold;
+    margin: 0px;
+    color: #40514e;
+    text-align: center;
+`;
