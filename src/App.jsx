3a7a3b20dbf916ac834b6fc284317247b595fa36
goro-ne <hayao56@gmail.com>
2024-08-02 22:36:38 +0900

    仮ヘッダー追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
 import { UserCard } from './components/organisms/user/UserCard';
+import { HeaderOnly } from './components/templates/HeaderOnly';
 
 const user = {
   name: "山田裕一",
@@ -19,13 +20,13 @@ const user = {
 function App() {
   return (
     <>
-      <div>
+      <HeaderOnly>
         <PrimaryButton>テスト</PrimaryButton>
         <SecondaryButton>検索</SecondaryButton>
         <br />
         <SearchInput />
         <UserCard user={user} />
-      </div>
+      </HeaderOnly>
     </>
   );
 }
---
+++ src/components/templates/DefaultLayout.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const DefaultLayout = (props) => {
+    const {} = props;
+    return (
+        <div>
+
+        </div>
+    );
+};
