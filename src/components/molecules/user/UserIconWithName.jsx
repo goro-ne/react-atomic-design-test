69462d5015ae3c3b5cddc3193c2a9e7530379930
goro-ne <hayao56@gmail.com>
2024-08-19 13:08:59 +0900

    memoで不要な再レンダリングを防ぐ

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,9 +1,11 @@
-import { useContext } from "react";
+import { memo, useContext } from "react";
 
 import styled from "styled-components";
 import { UserContext } from "../../../providers/UserProvider";
 
-export const UserIconWithName = (props) => {
+export const UserIconWithName = memo((props) => {
+    console.log("UserIconWithName");
+    
     const { image, name } = props;
     const { userInfo } = useContext(UserContext);
     const isAdmin = userInfo ? userInfo.isAdmin : false;
@@ -17,7 +19,7 @@ export const UserIconWithName = (props) => {
             { isAdmin && <SEdit>編集</SEdit>}
         </div>
     );
-};
+});
 
 const SContainer = styled.div`
     text-align: center;
